6a7009d97b971acf3fad4d625dcdf628f1a820df
Alastair Brown <alastair.brown@brighthr.com>
2019-11-24 11:02:05 +0000

    determine lenght of array of types rather than define it in code

--- getType.c
+++ getType.c
@@ -4,7 +4,6 @@
 #include "lwws.h"
 #include "getType.h"
 
-#define NUM_KNOWN_TYPES 5
 #define EXTENSION 0
 #define TYPE 1
 #define DEFAULT "default"
@@ -23,13 +22,11 @@ const char * getType(char * sExtension)
   LOGGER(10, "0,0 %s 0,1 %s 1,0 %s, 1,1 %s\n", sTypes[0][0], sTypes[0][1], sTypes[1][0], sTypes[1][1]);
   LOGGER(10, "...mapping extension %s to type\n", (char *) sExtension);
 
-  for(int i=0; i<NUM_KNOWN_TYPES; i++)
-  {
-    if (!strcmp(sTypes[i][EXTENSION], sExtension))
-    {
-      LOGGER(10, "...found a mapping for %s at index %d\n", sTypes[i][EXTENSION], i);
-      return(sTypes[i][TYPE]);
-    }
-  }
-  return ("text/html"); //default
+  int i=0;
+  while (strcmp(sTypes[i][EXTENSION], sExtension) && strcmp(sTypes[i][EXTENSION], DEFAULT))
+      i++;
+
+  LOGGER(10, "...found a mapping for %s at index %d\n", sTypes[i][EXTENSION], i);
+  return(sTypes[i][TYPE]);
+
 }
